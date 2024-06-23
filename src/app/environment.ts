fb444e692ed1026d0f2680312ac795beded6a4e2
Islam Jahedul <jahedul.islam@welldev.io>
2024-06-23 10:58:14 +0600

    update the env file

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,8 +1,12 @@
 
 export const protocol = 'http://';
+export const localhost = 'localhost';
+export const targetPort = '8080';
 export const host = '192.168.64.2';
+export const serviceName = 'busybox-backend-service';
 export const nodePort = '32500';
 export const apiPath = '/busyBox/api/v1';
 
 
-export const originalUrl = protocol + host + ':' + nodePort + apiPath;
+// export const originalUrl = protocol + host + ':' + nodePort + apiPath;
+export const originalUrl = protocol + serviceName + ':' + targetPort + apiPath;
