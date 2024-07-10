1518966cfd62d0436545af1608e0360ade5e6604
jahedul <jahedulislam6060@gmail.com>
2024-07-10 22:51:33 +0600

    change env file

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -5,9 +5,9 @@ export const targetPort = '8080';
 export const host = '192.168.64.3';
 export const serviceName = 'busybox-backend-service';
 export const nodePort = '32500';
-export const apiPath = '/busybox/api/v1';
+export const apiPath = '/busyBox/api/v1';
 
 
 // export const originalUrl = protocol + host + ':' + nodePort + apiPath;
 // export const originalUrl = protocol + serviceName + apiPath;
-export const originalUrl = "http://localhost:8080/busybox/api/v1";
+export const originalUrl = 'http://busybox-backend-service.default.svc.cluster.local:8080/busyBox/api/v1/tasks';
