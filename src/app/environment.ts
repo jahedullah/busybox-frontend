d090b029dc4ff17e3d660e2f5ebac9923a8a4ac8
Islam Jahedul <jahedul.islam@welldev.io>
2024-07-14 19:32:36 +0600

    update the path

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -11,4 +11,5 @@ export const apiPath = '/busyBox/api/v1';
 // export const originalUrl = protocol + host + ':' + nodePort + apiPath;
 // export const originalUrl = protocol + serviceName + apiPath;
 // export const originalUrl = 'http://busybox-backend-service.default.svc.cluster.local:8080/busyBox/api/v1';
-export const originalUrl = 'http://34.69.120.45:32500/busyBox/api/v1';
+// export const originalUrl = 'http://34.69.120.45:32500/busyBox/api/v1';
+export const originalUrl = 'http://localhost:8080/busyBox/api/v1';
